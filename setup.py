80803cab7d69f4851f3917cfc48d1e4540425233
Tinomutenda <tynoenyamhingura@gmail.com>
2024-05-29 09:59:02 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="TinomutendaNyamhingura",
+    author_email="tynoenyamhingura@gmail.com.com",
+    packages=find_packages(),
+    install_requires=[],
+)
