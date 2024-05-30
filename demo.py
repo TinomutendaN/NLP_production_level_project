afa14c16549e0281f49c3861de77614351037696
Tinomutenda <tynoenyamhingura@gmail.com>
2024-05-30 09:42:11 +0200

    cloud config

--- demo.py
+++ demo.py
@@ -1,15 +1,13 @@
 from hate.logger import logging
 from hate.exception import CustomException
 import sys
+from hate.configuration.gcloud_syncer import GCloudSync
+
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate_speech_2034","dataset.zip", "download/dataset.zip")
 
 
 
 
 
-#logging.info("Welcome to my project")
 
-try:
-    a= 7/ "0"
-
-except Exception as e:
-    raise CustomException(e , sys) from e 
