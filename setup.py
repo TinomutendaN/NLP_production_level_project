eddaa4f1590180cace0c9eaeda33f01bd6a1ba03
Tinomutenda <tynoenyamhingura@gmail.com>
2024-06-09 12:58:50 +0200

    adjustments

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="hate-speech-classification",
     version="0.0.1",
-    author="TinomutendaNyamhingura",
-    author_email="tynoenyamhingura@gmail.com.com",
+    author="iNeuron",
+    author_email="entbappy73@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
